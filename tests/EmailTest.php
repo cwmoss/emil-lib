aff2d195ce69e0a7a65a6d22b4b520aa778e3639
Robert Wagner <rw@20sec.net>
2023-06-28 19:40:50 +0200

    tests

--- tests/EmailTest.php
+++ tests/EmailTest.php
@@ -60,6 +60,26 @@ final class EmailTest extends TestCase {
         $this->assertStringContainsString("Good Day", $mime);
     }
 
+    public function testHtmlWithEmbedInLayout(): void {
+        $data = ['name' => 'otto', 'what' => 'fun', 'from' => '"Support" office@acme.com', 'to' => 'webmaster@localhost'];
+        $p = new processor('welcome', $this->opts);
+        // dd($p->run($data));
+        $email = $this->mailer->create_email(...$p->run($data));
+
+        $mime = $email->toString();
+        // dd($mime);
+        $this->assertStringContainsString('Content-Type: multipart/related', $mime);
+        $this->assertStringContainsString('Content-Type: text/html', $mime);
+        $this->assertStringContainsString('Content-Type: image/png', $mime);
+        $this->assertStringContainsString('Content-Disposition: inline', $mime);
+        $this->assertStringContainsString('@symfony', $mime);
+        $this->assertStringContainsString('cid:', $mime);
+
+        $this->assertStringContainsString('Subject: Welcome otto', $mime);
+        $this->assertStringContainsString('From: Support <office@acme.com>', $mime);
+        $this->assertStringContainsString("Thank you for registering", $mime);
+    }
+
     public function testBasicEmailTo(): void {
         $data = ['name' => 'otto', 'what' => 'fun', 'from' => '"Support" office@acme.com'];
         $mailer = new emil\mailer(['transport' => 'null://null'], $this->opts);
